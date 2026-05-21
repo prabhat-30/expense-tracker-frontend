36ee67272f68a68f403c5ec4193ee645041455fe
Prabhat Kumar <127616793+prabhat-30@users.noreply.github.com>
2026-05-22 00:29:20 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 // https://vite.dev/config/
 export default defineConfig({
